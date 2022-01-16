9f5b7bee46bf159de5ff202b565afd38c84552d9
Akshay1403 <86925930+Akshay1403@users.noreply.github.com>
2022-01-16 14:33:06 +0530

    Add files via upload

---
+++ RegularExpression.java
@@ -0,0 +1,14 @@
+import java.util.regex.*;
+
+public class RegularExpression {
+	 
+	public static void main(String [] args) {
+		String pattern = "[a-z] + ";
+		String Checker = "Regular Expression";
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(Checker);
+		while(m.find()) {
+			System.out.println(Checker.substring(m.start(), m.end()));
+		}
+	}
+}
