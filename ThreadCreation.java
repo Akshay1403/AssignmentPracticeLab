56ad930bcc43dac5db66ad958354bc34223e17a5
Akshay1403 <86925930+Akshay1403@users.noreply.github.com>
2022-01-16 14:45:21 +0530

    Add files via upload

--- ThreadCreation.java
+++ ThreadCreation.java
@@ -1,4 +1,13 @@
+public class ThreadCreation extends Thread {
 
-public class ThreadCreation {
-
-}
+	 	public void run()
+	 	{
+	  		System.out.println("concurrent thread started running..");
+	    }
+	 	
+	 	public static void main(String [] args)
+	 	{
+	  		ThreadCreation tc = new  ThreadCreation ();
+	  		tc.start();
+	 	}
+	}
